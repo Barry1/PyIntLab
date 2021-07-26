35a659bb005826f4f298720b6dd2b8f9f85b97e7
Ebeling, Dr. Bastian <bastian.ebeling@web.de>
2021-07-26 12:37:15 +0200

    setup.py and setup.cfg needed

---
+++ setup.py
@@ -0,0 +1,8 @@
+"""
+For development installation of the package pyintlab.
+
+some details where taken from <https://setuptools.readthedocs.io/en/latest/>
+"""
+import setuptools  # type: ignore[import]
+
+setuptools.setup()
