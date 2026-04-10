354e2cb8aca57b13b9c6bf94a56f2e5a94fb0574
Dr. Bastian Ebeling <230051+Barry1@users.noreply.github.com>
2026-04-10 20:05:49 +0200

    empfehlung future

--- src/pyintlab/__init__.py
+++ src/pyintlab/__init__.py
@@ -1,6 +1,8 @@
 """Needed to explain that this is a package."""
 
+from __future__ import annotations
+
 from .numpydt_scalar_interval import NPScalarInterval
 from .scalar_interval import ScalarInterval
 
-__all__ = ["ScalarInterval", "NPScalarInterval"]
+__all__: list[str] = ["ScalarInterval", "NPScalarInterval"]
